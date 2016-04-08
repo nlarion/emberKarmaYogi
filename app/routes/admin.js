618fd43dd3740f1213fb65b5309c7e8d1684eb9f
Neil Larion <neil.larion@gmail.com>
2016-04-08 11:45:00 -0700

    Add new components for user city and state

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+  saveStudio(params){
+    var newProduct = this.store.createRecord('product', params);
+    var user = params.user;
+    user.get('products').addObject(newProduct);
+    newProduct.save().then(function(){
+      return user.save();
+    });
+
+    this.transitionTo('store');
+  },
+  saveUser(params){
+    var newUser = this.store.createRecord('user', params);
+    newUser.save();
+    this.transitionTo('admin');
+  }
+}
 });
