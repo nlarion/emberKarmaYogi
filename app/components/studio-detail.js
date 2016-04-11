7183d0295a4cf955912d69c926efb93da7d8b0c6
Neil Larion <neil.larion@gmail.com>
2016-04-11 00:46:10 -0700

    add computed property to display studio name and description

--- app/components/studio-detail.js
+++ app/components/studio-detail.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  studioDescription: Ember.computed('studio.name', 'studio.description', function() {
+    var name = this.get('studio.name');
+    var description = this.get('studio.description');
+    return name + " " + description
+  }),
 });
