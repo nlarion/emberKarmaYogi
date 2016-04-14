484910098a4112ebcb55d98a0abe311bf186bd18
Neil Larion <neil.larion@gmail.com>
2016-04-14 10:48:37 -0700

    add helper to display how many ratings from users there are on the studio page

--- app/helpers/rating-score.js
+++ app/helpers/rating-score.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
-export function ratingScore(params/*, hash*/) {
-  console.log(params[0]);
-  return params;
+export function ratingScore(params) {
+  var totalRatings = 0;
+  var test = params[0].get('ratings').forEach(function(prop){
+    totalRatings++;
+  });
+
+
+  return "This studio has "+totalRatings+ " ratings from users";
 }
 
 export default Ember.Helper.helper(ratingScore);
